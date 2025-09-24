28231c7ae5919dccb037e04c903d394f884f5e21
Maxence <lemaila.maxence@gmail.com>
2025-09-24 21:18:33 +0200

    unused code removal

--- tp_bluetooth_serveur/models/Temp.js
+++ tp_bluetooth_serveur/models/Temp.js
@@ -2,8 +2,6 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/objetsconnectes')
     .then(() => console.log('✅ Connecté à MongoDB'))
     .catch(err => console.error('❌ Erreur connexion MongoDB :', err));
-mongoose.Promise = global.Promise
-mongoose.connection.on('error', console.error.bind(console, 'Une erreur de connexion MongoDB est survenue :'))
 
 
 const tempSchema = new mongoose.Schema({
